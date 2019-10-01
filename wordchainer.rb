cbab456ac7460911592e03ce62e8fb3e47e5be11
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-09-30 20:52:06 -0600

    Finished

--- wordchainer.rb
+++ wordchainer.rb
@@ -1,55 +1,68 @@
+require 'set'
+
 class WordChainer
+  attr_reader :dictionary
 
-    attr_reader :dictionary
+  def initialize(dictionary_file_name='dictionary.txt')
+    @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
+    @dictionary = Set.new(@dictionary)
+  end
 
-    def initialize(dictionary_file_name="dictionary.txt")
-        words = File.readlines(dictionary_file_name).map(&:chomp)
-        #set.include? method is much faster than that of an array
-        @dictionary = Set.new(words)
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = { source => nil }
+
+    until @current_words.empty? || @all_seen_words.include?(target)
+      explore_current_words
     end
 
-    def adjacent_word(word)
-        #We want to give it a word and return any word that is adjacent based on any
-        #letter being changed out for another while still being in dict
-        list_words = []
-        alphabet = ("a".."z").to_a
-        word.each_char.with_index do |char, index|
-            alphabet.each do |letter|
-                word[index] = letter
-                new_word = word[0...index] + word[index] + word[index+1..-1]
-                if @dictionary.include?(new_word)
-                    list_words << new_word
-                end
-            end
-        end
-        return list_words
+    build_path(target)
+  end
+
+  def adjacent_words(word)
+    adjacent_words = []
+
+    word.each_char.with_index do |old_letter, i|
+      ('a'..'z').each do |new_letter|
+        next if old_letter == new_letter
+
+        new_word = word.dup
+        new_word[i] = new_letter
+
+        adjacent_words << new_word if dictionary.include?(new_word)
+      end
     end
 
-    def run(source)
-        current_words = [source]
-        all_seen_words = [source]
+    adjacent_words
+  end
 
-        until current_words.length == 0
-            new_current_words = []
+  def explore_current_words
+    new_current_words = []
+    @current_words.each do |current_word|
+      adjacent_words(current_word).each do |adjacent_word|
+        next if @all_seen_words.key?(adjacent_word)
 
-            current_words.each do |ele|
-
-                adjacent_word(ele).each do |ele2|
-                    if all_seen_words.include?(ele2) == false
-                        new_current_words << ele2
-                        all_seen_words << ele2
-                    else
-                        next
-                    end
-                end
-                
-            end
-
-            puts new_current_words
-            current_words = new_current_words
-        end
-
-        return all_seen_words
+        new_current_words << adjacent_word
+        @all_seen_words[adjacent_word] = current_word
+      end
     end
 
+    @current_words = new_current_words
+  end
+
+  def build_path(target)
+    path = []
+    current_word = target
+    until current_word.nil?
+      path << current_word
+      current_word = @all_seen_words[current_word]
+    end
+
+    path.reverse
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  # provide file name on command line
+  p WordChainer.new(ARGV.shift).run("duck", "ruby")
 end
